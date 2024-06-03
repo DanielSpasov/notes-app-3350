3784865fe7cd8280cfe4e980663679b9a8292e8e
Daniel Spasov <shadygotrabies@gmail.com>
2024-06-03 12:04:28 +0300

    Update Note component behavior

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { FC } from 'react';
 
 import { INote } from '../types/Note';
@@ -10,17 +11,22 @@ type NoteProps = {
 
 const Note: FC<NoteProps> = ({ note, onRemove }) => {
   return (
-    <article className="w-64 h-64 shadow-sm shadow-neutral-400 rounded-md mx-3">
-      <header className="flex justify-between items-center bg-yellow-300 p-2 rounded-t-md">
-        <h1>{note.title}</h1>
+    <article className="flex flex-col min-w-64 w-64 min-h-64 max-h-64 shadow-sm shadow-neutral-400 rounded-md m-3 hover:scale-105">
+      <header className="flex justify-between rounded-t-md bg-yellow-300">
+        <Link
+          to={`${note.id}`}
+          className="p-2 whitespace-nowrap overflow-hidden overflow-ellipsis hover:underline"
+        >
+          {note.title}
+        </Link>
+
         <Icon
           model="close"
-          className="p-1 w-7 h-7 hover:bg-white rounded-md cursor-pointer"
+          className="my-2 mr-2 p-1 min-w-6 min-h-6 rounded-md cursor-pointer hover:bg-white"
           onClick={() => onRemove(note)}
         />
       </header>
-
-      <p className="p-2">{note.content}</p>
+      <p className="pl-2 h-full break-words overflow-y-auto">{note.content}</p>
     </article>
   );
 };
