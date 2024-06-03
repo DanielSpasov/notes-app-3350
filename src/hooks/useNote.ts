a5096e69c606145c97112f027be2a36ef89a5c45
Daniel Spasov <shadygotrabies@gmail.com>
2024-06-03 15:49:54 +0300

    Created functional pagination for the home page

--- src/hooks/useNote.ts
+++ src/hooks/useNote.ts
@@ -2,35 +2,58 @@ import { useCallback, useEffect, useState } from 'react';
 import { INote } from '../types/Note';
 
 const useNotes = () => {
-  const [notes, setNotes] = useState<INote[]>(
+  const [page, setPage] = useState(
+    Number(JSON.parse(localStorage.getItem('page') || '1'))
+  );
+
+  const [allNotes, setAllNotes] = useState<INote[]>(
     JSON.parse(localStorage.getItem('notes') || '[]')
   );
-
-  useEffect(
-    () => localStorage.setItem('notes', JSON.stringify(notes)),
-    [notes]
+  const [notes, setNotes] = useState<INote[]>(
+    allNotes.slice(page * 15 - 15, page * 15)
   );
 
+  useEffect(() => {
+    setNotes(allNotes.slice(page * 15 - 15, page * 15));
+  }, [page, allNotes]);
+
+  // Local Storage Updates
+  useEffect(() => {
+    localStorage.setItem('page', JSON.stringify(page));
+  }, [page]);
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(allNotes));
+  }, [allNotes]);
+
   const onAdd = useCallback(
-    (note: INote) => setNotes(prev => [...prev, note]),
+    (note: INote) => setAllNotes(prev => [...prev, note]),
     []
   );
 
   const onRemove = useCallback(
-    (note: INote) => setNotes(prev => prev.filter(n => n.id !== note.id)),
+    (note: INote) => setAllNotes(prev => prev.filter(n => n.id !== note.id)),
     []
   );
 
   const get = useCallback(
-    (id: string) => notes.find(note => note.id === id),
-    [notes]
+    (id: string) => allNotes.find(note => note.id === id),
+    [allNotes]
   );
 
   return {
     notes,
     get,
     onAdd,
-    onRemove
+    onRemove,
+    pagination: {
+      page,
+      canNext: page * 15 < allNotes.length,
+      canPrev: page > 1,
+      onNext: () => setPage(prev => prev + 1),
+      onPrev: () => setPage(prev => prev - 1),
+      onFirst: () => setPage(1),
+      onLast: () => setPage(Math.ceil(allNotes.length / 15))
+    }
   };
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,11 @@ import useNotes from '../hooks/useNote';
 
 import PlusIcon from '../svgs/Plus';
 
+const buttonStyle =
+  'px-2 border-2 outline-none bg-neutral-200 hover:bg-neutral-300 disabled:opacity-50';
+
 const Home = () => {
-  const { notes, onAdd, onRemove } = useNotes();
+  const { notes, onAdd, onRemove, pagination } = useNotes();
 
   const [draft, setDraft] = useState(false);
 
@@ -24,17 +27,54 @@ const Home = () => {
         {draft ? (
           <Draft setDraft={setDraft} onAdd={onAdd} noteRef={noteRef} />
         ) : (
-          <PlusIcon
-            className="fixed bottom-0 right-0 sm:relative bg-neutral-50 w-12 h-12 p-3 m-4 shadow-sm shadow-neutral-400 hover:bg-neutral-100 mx-3 rounded-md cursor-pointer"
-            onClick={() => {
-              setDraft(true);
-              requestAnimationFrame(() => {
-                noteRef.current?.scrollIntoView({ behavior: 'smooth' });
-              });
-            }}
-          />
+          !pagination.canNext && (
+            <PlusIcon
+              tabIndex={0}
+              className="fixed bottom-0 right-0 sm:relative bg-neutral-50 w-12 h-12 p-3 m-4 shadow-sm shadow-neutral-400 hover:bg-neutral-100 mx-3 rounded-md cursor-pointer"
+              onClick={() => {
+                setDraft(true);
+                requestAnimationFrame(() => {
+                  noteRef.current?.scrollIntoView({ behavior: 'smooth' });
+                });
+              }}
+            />
+          )
         )}
       </article>
+
+      <footer className="p-4 text-lg bottom-0 w-full flex justify-center">
+        <button
+          disabled={pagination.page === 1}
+          onClick={pagination.onFirst}
+          className={`rounded-l-xl ${buttonStyle}`}
+        >
+          &lt;&lt; First
+        </button>
+        <button
+          disabled={!pagination.canPrev}
+          onClick={pagination.onPrev}
+          className={buttonStyle}
+        >
+          &lt; Prev
+        </button>
+
+        <p className="border-y-2 p-2 px-4">{pagination.page}</p>
+
+        <button
+          disabled={!pagination.canNext}
+          onClick={pagination.onNext}
+          className={buttonStyle}
+        >
+          Next &gt;
+        </button>
+        <button
+          disabled={!pagination.canNext}
+          onClick={pagination.onLast}
+          className={`rounded-r-xl ${buttonStyle}`}
+        >
+          Last &gt;&gt;
+        </button>
+      </footer>
     </main>
   );
 };
